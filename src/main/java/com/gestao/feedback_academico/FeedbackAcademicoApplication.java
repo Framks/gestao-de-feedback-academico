4b3c004afe79051c85324aad8f40da7b1a4a139c
Francisco <galves09@gmail.com>
2024-07-29 15:21:04 -0300

    add dependências

---
+++ src/main/java/com/gestao/feedback_academico/FeedbackAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.feedback_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao/feedback_academico/FeedbackAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.feedback_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
