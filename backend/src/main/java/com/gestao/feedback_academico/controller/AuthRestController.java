63cd82c3be15ffcf2dda0d21df2cbcbfb67757d8
Francisco <galvesa9@gmail.com>
2024-09-16 13:03:20 -0300

    criando o deploy

--- backend/src/main/java/com/gestao/feedback_academico/controller/AuthRestController.java
+++ backend/src/main/java/com/gestao/feedback_academico/controller/AuthRestController.java
@@ -8,6 +8,7 @@ import com.gestao.feedback_academico.domain.repository.UserRespository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +21,18 @@ public class AuthRestController {
     private final UserRespository userRespository;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthRestController(UserRespository userRespository, AuthenticationManager authenticationManager, JwtUtil jwtService) {
+    public AuthRestController(UserRespository userRespository, AuthenticationManager authenticationManager, JwtUtil jwtService, PasswordEncoder passwordEncoder) {
         this.userRespository = userRespository;
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping
     public String login(@RequestBody AuthRequest authRequest) {
+        System.out.println(authRequest.senha()+ " encode \n "+ this.passwordEncoder.encode(authRequest.senha()));
         Authentication authentication = new UsernamePasswordAuthenticationToken(authRequest.email(), authRequest.senha());
         this.authenticationManager.authenticate(authentication);
         User usuario = this.userRespository.findByEmail(authRequest.email()).orElseThrow(() -> new RuntimeException("Error na busca de um usuario"));
---
+++ backend/src/test/java/com/gestao/feedback_academico/service/TurmaServiceTest.java
@@ -0,0 +1,21 @@
+package com.gestao.feedback_academico.service;
+
+import com.gestao.feedback_academico.domain.usecase.TurmaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class TurmaServiceTest {
+
+    private TurmaService turmaService;
+
+    @BeforeEach
+    public void setup(){
+        this.turmaService = Mockito.mock(TurmaService.class);
+    }
+
+    @Test
+    public void findAll(){
+
+    }
+}
